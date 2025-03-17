270588b647a2201bc28d62aa499c7e3072117124
naimdeb <naimdebard02@gmail.com>
2025-03-17 14:12:03 +0100

    added board and squares

---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import "./Square.css";
+
+
+function Square() {
+
+    const [value, setValue] = useState(null)
+
+    function handleClick(){
+        setValue("X");
+        
+    }
+
+
+
+  return (
+    <button className="square" onClick={handleClick}>{value}</button>
+  )
+}
+
+export default Square
